8087a780e71ac3f69c45342d730fa9a877fdd6b6
Trevis McGill <trevismcgill25@gmail.com>
2020-07-30 02:26:41 +0000

    Done.

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,57 @@
 
+def sort_array_asc(array)
+  array.sort do |a, b|
+  a <=> b
+  end
+end
+
+def sort_array_desc(array)
+  array.sort do |a, b|
+    b <=> a
+  end
+end
+
+def sort_array_char_count(array)
+  array.sort do |a, b|
+    a.length <=> b.length
+  end
+end
+
+def swap_elements(array)
+  array[1], array[2] = array[2], array[1]
+  return array
+end
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |cash|
+    cash[2] = "$"
+  end
+end
+
+def find_a(array)
+  a_array = []
+  array.each do |a_test|
+    if a_test[0] == "a"
+      a_array.push(a_test)
+    end
+  end
+    return a_array
+end
+
+def sum_array(array)
+  array.inject(0, &:+)
+end
+
+def add_s(array)
+  array.each_with_index do |item, index|
+    if index == 1
+    nil
+    else
+      item << "s"
+    end
+  end
+end
